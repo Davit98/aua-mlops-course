81a5fb9c3071f77ca407a2d2a44a400485459509
Davit Papikyan <davidpyan3@gmail.com>
2023-02-26 21:25:03 +0400

    add pss materials

--- pss/session_05/source/softmax.py
+++ pss/session_05/source/softmax.py
@@ -9,7 +9,7 @@ def stable_softmax(x: Union[np.ndarray, torch.Tensor],
     
     Arguments:
         x: Either numpy array or torch tensor.
-        const: Constant value.
+        const: Constant value or an array/tensor of constants.
         
     Returns:
         softmax(x-const).
@@ -18,12 +18,10 @@ def stable_softmax(x: Union[np.ndarray, torch.Tensor],
         z = x - const
         numerator = np.exp(z)
         denominator = np.sum(numerator, axis=-1, keepdims=True)
-        softmax = np.exp(z) / denominator
     elif isinstance(x, torch.Tensor):
         z = x - const
         numerator = z.exp()
         denominator = numerator.sum(dim=-1, keepdims=True)
-        softmax = numerator / denominator
     else:
         raise ValueError(f"Expected x to be of type either np.ndarray or torch.Tensor but got {type(x)}.")
-    return softmax
+    return numerator / denominator
